1afd5b677c9ed2a88c6a7c710f0e39c7fef21a32
githubmeking <130077291+githubmeking@users.noreply.github.com>
2024-10-23 02:25:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
+SESSION_NAME = getenv("SESSION_NAME", "BQDNvmIAZEOhE36eqs5X2MCzdiFC8Bz2eP1PRXQVvIex63cJmWjQBTyMgD1zLQ1iG6NodFwCdpfMeLNa5uWJpLzYU5jV6uQoCVxAKDMO-45mJdUQhLC1YJLxnZvkm5bMIJna7TTzThU_anybVymqBu3MnXLsNzEMi9YwjsnhjyMpy9Sr-qb6voFEoKRqaQHwFG-axYHNj0TaHAzXMBfxXNDHHsFG1TunQlT8MeHmjsk1z5fZyuScTYTaN5w736c0XKbpUynRKoKkPJQM4NmFtdES4HSVwhvm3HsgTolCm1Nk-1nEd7jHp__yyms_CDQr21MnuhSbp77ZhP-KNL82_Mf2cDFZJwAAAAEqb5LmAA")
+BOT_TOKEN = getenv("BOT_TOKEN","8197433189:AAEBLi_ma4UXrrSzBhchJyM_ccPJAFhGBIQ")
 BOT_NAME = getenv("BOT_NAME", "Talia Müzik Projesi")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/cd0b87484429704c7b935.png")
 THUMB_IMG = getenv("THUMB_IMG", "https://i.ibb.co/VQd4HSH/Photo-1629477903701.jpg")
@@ -17,12 +17,12 @@ QUE_IMG = getenv("QUE_IMG", "https://i.ibb.co/VQd4HSH/Photo-1629477903701.jpg")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Mp3dinleme_Bot ")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "SesMusicAsistan")
+BOT_USERNAME = getenv("BOT_USERNAME", "@Denek99bot ")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Onedio Asistan1")
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Sohbetdestek")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "EfsaneMusicProject")
-OWNER_NAME = getenv("OWNER_NAME", "Mahoaga") # kullanıcı adınızı semboller olmadan doldurma @
-DEV_NAME = getenv("DEV_NAME", "mahoaga")
+OWNER_NAME = getenv("OWNER_NAME", "@hurkole") # kullanıcı adınızı semboller olmadan doldurma @
+DEV_NAME = getenv("DEV_NAME", "hurkole")
 PMPERMIT = getenv("PMPERMIT", None)
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "250"))
